e1572170fd23da53be346ab703883c69fd4bf3e6
ltginrage <97143303+xem0861@users.noreply.github.com>
2022-07-15 19:07:25 -0500

    moving website back
    Moving website back from repl. Wasnt enjoying it. GitHub is better

---
+++ script1.js
@@ -0,0 +1,12 @@
+
+const countEl = document.getElementById("count")
+
+updateVisitCount()
+
+function updateVisitCount() {
+    fetch("https://api.countapi.xyz/update/visitcounter/cantcode?amount=1")
+        .then(response => response.json())
+        .then(response => {
+            countEl.innerHTML = response.value
+        })
+}
